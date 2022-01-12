b58fa90e076a7808effbfad69b80b9f2926e7894
rizalkhi <rizal@samelement.com>
2022-01-12 21:23:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <the-header title="Remainder"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+// import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/Layouts/TheHeader.vue';
+export default {
+  components: {
+    // LearningResource,
+    TheHeader,
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'random-id-22141',
+          title: 'Official vuejs tutor',
+          description: 'this is course vuejs',
+          link: 'http://www.github.com/rizalkhisyam',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,25 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resources')"
+      >Add Resources</base-button
+    >
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
